fd6a7936975aef79939b649cd6a0e314aca816ed
Ekam Ahuja <ekamahujau@gmail.com>
2021-10-02 17:58:02 +0000

    Fixed bugs with status update system

--- classes/order.rb
+++ classes/order.rb
@@ -11,14 +11,14 @@ class Order
 
     if order_response[:response_code] === 200
       order_data =  {
-          "user_id": Account.get_logged_in_user['id'],
+          "user_id": 2, #Account.get_logged_in_user['id']
           "order_id": Utils.get_amount_of_orders + 1.to_i,
           "provider_order_id": JSON.parse(order_response[:response])['order'],
           "service_id": @order_service.to_i,
           "service_name": Utils.get_services([@order_service])[0]['name'],
           "link": @order_link,
           "quantity": @order_quantity.to_i,
-          "status": order_status['status'].downcase,
+          "status": order_status['status'],
           "start_count": order_status['start_count'],
           "remains": order_status['remains'].to_i,
           "charge": order_status['charge'].to_i,
@@ -37,17 +37,18 @@ class Order
     rows = []
     orders = Utils.fetch_orders
     user_id = Account.get_logged_in_user['id']
-    orders.each do |order|
+    orders.each.with_index do |order, i|
       if order['user_id'] === user_id
         if ['pending', 'processing', 'progress', nil].include? order['status']
           status = Order.check_status(order['provider_order_id'])
+          orders[i]['status'] = status.downcase
           rows << [order['order_id'], order['service_name'], order['link'], order['charge'], order['start_count'], order['quantity'], status['status'], order['remains']]
         else
-          rows << [order['order_id'], order['service_name'], order['link'], order['charge'], order['start_count'], order['quantity'], order['status'].capitalize, order['remains']]
+          rows << [order['order_id'], order['service_name'], order['link'], order['charge'], order['start_count'], order['quantity'], order['status'], order['remains']]
         end
       end
     end
-
+    File.write(JSON.parse(File.read './storage/config/system_config.json')['ORDERS'], JSON.pretty_generate(orders))
     table = Terminal::Table.new :headings => ['Order ID', 'Service Name' , 'Link', 'Charge', 'Start Count', 'Quantity', 'Status', 'Remains'], :rows => rows
     puts table
   end
