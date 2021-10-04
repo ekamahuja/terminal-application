3b98bb9cf1e0e43b79df6672db0edfda782aed19
Ekam Ahuja <ekamahujau@gmail.com>
2021-10-04 12:08:53 +0000

    Minior changes and cleanup

--- start.sh
+++ start.sh
@@ -2,27 +2,38 @@
 
 bundle install
 
-API_KEY=c2623c04dcd24d138824a8d8c8177446
+API_KEY=somekey
 
-if [! -f ./storage/dataBase/accounts.json]
+mkdir -p ./storage/dataBase/
+
+if [ ! -f ./storage/dataBase/accounts.json ]
 then
 	touch ./storage/dataBase/accounts.json
 	echo [] > ./storage/dataBase/accounts.json
+elif [ -s ./storage/dataBase/accounts.json ]
+then
+	echo [] > ./storage/dataBase/accounts.json
 fi
 
-if [! -f ./storage/dataBase/orders.json]
+if [ ! -f ./storage/dataBase/orders.json ]
 then
 	touch ./storage/dataBase/orders.json
 	echo [] > ./storage/dataBase/orders.json
+elif [ -s ./storage/dataBase/orders.json ]
+then
+	echo [] > ./storage/dataBase/orders.json	
 fi
 
-if [! -f ./storage/dataBase/services.json]
+if [ ! -f ./storage/dataBase/services.json ]
 then
 	touch ./storage/dataBase/services.json
 	echo [] > ./storage/dataBase/services.json
+elif [ -s ./storage/dataBase/orders.json ]
+then
+	echo [] > ./storage/dataBase/orders.json	
 fi
 
-if [! -f ./.env]
+if [ ! -f ./.env ]
 then
 	touch ./.env
 	echo $API_KEY > ./.env
