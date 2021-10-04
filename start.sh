3182be4154e34873a657eb8408c42f0a7484b93e
Ekam Ahuja <ekamahujau@gmail.com>
2021-10-04 10:30:09 +0000

    Updated start.sh file

--- start.sh
+++ start.sh
@@ -7,22 +7,25 @@ API_KEY=c2623c04dcd24d138824a8d8c8177446
 if [! -f ./storage/dataBase/accounts.json]
 then
 	touch ./storage/dataBase/accounts.json
+	echo [] > ./storage/dataBase/accounts.json
 fi
 
 if [! -f ./storage/dataBase/orders.json]
 then
 	touch ./storage/dataBase/orders.json
+	echo [] > ./storage/dataBase/orders.json
 fi
 
 if [! -f ./storage/dataBase/services.json]
 then
 	touch ./storage/dataBase/services.json
+	echo [] > ./storage/dataBase/services.json
 fi
 
 if [! -f ./.env]
 then
 	touch ./.env
-	cat $API_KEY > ./.env
+	echo $API_KEY > ./.env
 fi
 
 ruby index.rb
